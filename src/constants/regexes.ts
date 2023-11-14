658fa7398a1e902200768158305876c7e4415b55
Oleg <46903190+oleg1994@users.noreply.github.com>
2023-11-14 11:02:53 +0200

    changed the project structure to fit hosting structure 2

--- src/constants/regexes.ts
+++
@@ -1,4 +0,0 @@
-export const tiktokRegex =
-  /https:\/\/(?:m|www|vm|vt)?.?tiktok.com\/((?:.*\b(?:(?:usr|v|embed|user|video)\/|\?shareId=|&item_id=)(\w+))|\w+)/g;
-
-export const twitterRegex = /twitter\.com\/([^ ]+)/g;
--- src/constants/urls.ts
+++
@@ -1,3 +0,0 @@
-export const proxitok = "https://proxitok.pabloferreiro.es/";
-export const nitter = "https://nitter.net";
-export const twitterUrl = "twitter.com";
